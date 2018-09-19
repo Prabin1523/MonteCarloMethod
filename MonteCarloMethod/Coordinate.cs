d865cdaee28415923d8e766674028e54f8486aac
Prabin Pun <prabin1punah@gmail.com>
2018-09-19 00:02:28 -0400

    Add project files.

---
+++ MonteCarloMethod/Coordinate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MonteCarloMethod.Program;
+
+namespace MonteCarloMethod
+{
+
+
+    public struct Coordinate
+    {
+
+        private double x, y;
+
+        public double X { get => x; set => x = value; }
+        public double Y { get => y; set => y = value; }
+
+        //public double X { get; set; }
+        // public double Y { get; set; }
+
+        public Coordinate(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Coordinate(Random random)
+        {
+            this.x = random.NextDouble();
+            this.y = random.NextDouble();
+
+        }
+        public double Hypotenuse()
+        {
+            return Math.Sqrt(Math.Pow(this.X, 2) + Math.Pow(this.Y, 2));
+        }
+    }
+}
+
+    //square root of x^2 + y^2
+
+
---
+++ MonteCarloMethod/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MonteCarloMethod
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                Console.WriteLine("Enter a number: ");
+                int arrayLength = Convert.ToInt32(Console.ReadLine());
+                Coordinate[] arrayOfCoordinate = new Coordinate[arrayLength];
+                Random random = new Random();
+                int counter = 0;
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    arrayOfCoordinate[i] = new Coordinate(random);
+                    if (arrayOfCoordinate[i].Hypotenuse() <= 1)
+                    {
+                        counter++;
+                    }
+                }
+
+                double avg = (counter / (double)arrayOfCoordinate.Length) * 4;
+                Console.WriteLine($"Total # of points overlapping the circle = {counter}");
+                Console.WriteLine($"generated pi = {avg}, real pi = {Math.PI}, difference = {Math.Abs(Math.PI - avg) }");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+
+            }
+        }
+
+
+
+    }
+
+
+
+
+
+}
+
+
